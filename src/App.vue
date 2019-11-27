71306c5be7c6f18e5057760e21b9ee1ecf016b9d
PKXZ <524572319@qq.com>
2019-11-27 23:40:00 +0800

    tj

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <!--全局配置国际化文案-->
+  <a-locale-provider :locale="locale">
+    <div id="app">
+      <router-view/>
+    </div>
+  </a-locale-provider>
+</template>
+<script>
+  import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN';
+  export default {
+    data(){
+      return{
+        locale: zhCN
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import antDesignVue from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css';
+Vue.use(antDesignVue);
+
+import echarts from 'echarts'
+import 'echarts/theme/macarons.js'
+Vue.prototype.$echarts = echarts;
+import axios from './config/axiosConfig'
+import baseUrl from './config/api';
+Vue.prototype.api = baseUrl;
+Vue.use(axios, axios)
+Vue.prototype.axios = axios;
+
+import './assets/font/iconfont.css'
+import './assets/css/common.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
